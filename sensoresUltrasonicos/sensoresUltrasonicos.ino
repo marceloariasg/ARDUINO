ac4b0ff15e34315c6a92292da738ddba445641d5
Windows Notebook HP <marceloariasg@gmail.com>
2017-12-01 21:56:42 -0300

    ok

--- sensoresUltrasonicos/sensoresUltrasonicos.ino
+++ sensoresUltrasonicos/sensoresUltrasonicos.ino
@@ -1,42 +1,47 @@
 /*
-Configurar Nodemcu ESP8266 en Arduino
-https://www.youtube.com/watch?v=tCx9Byn36jw&list=PLw_hyusX2ghYbPmsGD_EsdVHKHHe57w8n
-
+sensor hc-sr04 con arduino
+https://www.youtube.com/watch?v=IF1eN0WK3bU
 */
-const int D1 = 5;
-const int D2 = 4;
-const int D5 = 14;
-int tiempo = 100;
+
+
+#define Pecho 6
+#define Ptrig 7
+long duracion, distancia;
 
 void setup() {
 
   Serial.begin(9600);   //iniciamos la comunicacion serial con una tasa de 9600 bps (bits por segundo)
-  pinMode(D1, OUTPUT);  //usamos el pin D1 LED VERDE
-  pinMode(D2, OUTPUT);  //usamos el pin D2 LED AMARILLO
-  pinMode(D5, OUTPUT);  //usamos el pin D5 LED ROJO
+  pinMode(Pecho, INPUT);  //Define el pin 6 como entrada (echo) 
+  pinMode(Ptrig, OUTPUT);  //Define el pin 7 como salida (triger)
+  //pinMode(13,1);          //Define el pin 13 como salida (led)
   
 }
 
 void loop() {
 
-    Serial.println("encendemos el led Verde");
-    digitalWrite(D1, HIGH);
-    delay(tiempo);   //espera 1 segundo
-    Serial.println("apagamos el led Verde");
-    digitalWrite(D1, LOW);
-    //delay(1000);   //espera 1 segundo
-    Serial.println("encendemos el led Amarillo");
-    digitalWrite(D2, HIGH);
-    delay(tiempo);   //espera 1 segundo
-    Serial.println("apagamos el Amarilo");
-    digitalWrite(D2, LOW);
-    //delay(1000);   //espera 1 segundo
-    Serial.println("encendemos el Rojo");
-    digitalWrite(D5, HIGH);
-    delay(tiempo);   //espera 1 segundo
-    Serial.println("apagamos el Rojo");
-    digitalWrite(D5, LOW);
-    //delay(1000);   //espera 1 segundo
-    
-    
+  digitalWrite(Ptrig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(Ptrig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(Ptrig, LOW);
+  
+  duracion = pulseIn(Pecho, HIGH);
+  distancia = (duracion/2) / 29;  //calcula la ditancia en centimetros
+
+  if (distancia >= 500 || distancia <= 0 ) {  //si la distancia es mayor a 500cm o menor a 0cm no mide nada
+      Serial.println("---");
+    }
+   else {
+      Serial.print(distancia);
+      Serial.println("cm");
+      digitalWrite(13,0);
+    }
+
+  if (distancia <= 10 && distancia >= 1 ) {  //en alto el led se la ditancia es menor a 10 y mayor igual a 1 
+      Serial.println("---");
+      digitalWrite(13,1);
+      Serial.println("Alarma......");      
+    }
+    delay(400);   //espera 4 segundos para que se logre ver la distancia
+
 }
